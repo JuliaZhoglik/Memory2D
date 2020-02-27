25465efc6b234c2306b0ff3d4156e95cdeac2b79
Julia Zhoglik <JuliaZhoglik@users.noreply.github.com>
2020-02-27 23:09:05 +0700

    Add game memory

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private SceneController controller;
+    [SerializeField] private GameObject cardBack;
+
+    public int _id;
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf && controller.canReveal) // объект активен, виден
+        {
+            cardBack.SetActive(false); // делаем объект не активным, невидимым
+            controller.CardRevealed(this);
+        }
+    }
+    
+    public int id {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Memory2D
+Game Memory 2D from book "Unity in action". Unity 2018.4.14f1
+
+Main
+![Main](https://github.com/JuliaZhoglik/Memory2D/raw/master/_images/main.png)
